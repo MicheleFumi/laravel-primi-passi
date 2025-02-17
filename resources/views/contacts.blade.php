b83fec8caee3a0854fb98d28487c12f83e6a1059
MicheleFumi <michele.fumi96@gmail.com>
2025-02-18 00:44:50 +0100

    add contacts and homepage

---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+        crossorigin="anonymous" />
+</head>
+
+<body>
+    <header>
+        <!-- place navbar here -->
+        <h1>this is a test!</h1>
+    </header>
+    <main>
+        <h2>hello world! welcome to contact page!</h2>
+        <h3>other pages</h3>
+        <a href="{{ Route('home') }}">click me!</a>
+    </main>
+    <footer>
+        <!-- place footer here -->
+    </footer>
+
+
+</body>
+
+</html>
